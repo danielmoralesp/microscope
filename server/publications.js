0eab533d7a5ced6bf0d181b4ccb3d765432390ac
danielmoralesp <daniel_moralesp@hotmail.com>
2016-09-10 19:05:56 -0500

    Creado un mecanismo simple de publicación/suscripción para los comentarios

--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,13 @@ Meteor.publish('posts', function() {
   return Posts.find();
 });
 
+/* PUBLISH ANTES DE COMENTARIOS CON SEGURIDAD
 Meteor.publish('comments', function() {
   return Comments.find();
 });
+*/
 
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
+});
